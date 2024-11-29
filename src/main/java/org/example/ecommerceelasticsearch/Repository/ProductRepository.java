8a887dd3db22f9f3b746758074d3d8ec4e60132c
Aniketgurjar <124706912+Aniketgurjar@users.noreply.github.com>
2024-11-30 00:49:54 +0530

    Update ProductRepository.java

--- src/main/java/org/example/ecommerceelasticsearch/Repository/ProductRepository.java
+++ src/main/java/org/example/ecommerceelasticsearch/Repository/ProductRepository.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public interface ProductRepository extends ElasticsearchRepository<Product, String> {
 
-    // Find products by name
+  
     List<Product> findByName(String name);
 
-    // Find products with price greater than a specific value
+   
     List<Product> findByPriceGreaterThan(double price);
 
-    // Find products with sorting by price
+   
     List<Product> findByNameContaining(String name, Sort sort);
 }
