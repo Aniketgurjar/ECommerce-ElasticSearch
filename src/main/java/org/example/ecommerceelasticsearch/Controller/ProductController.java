3da3ac4a32908aa9f94fa8e42a915b5cd5b835f3
Aniketgurjar <124706912+Aniketgurjar@users.noreply.github.com>
2024-11-30 00:50:35 +0530

    Update ProductController.java

--- src/main/java/org/example/ecommerceelasticsearch/Controller/ProductController.java
+++ src/main/java/org/example/ecommerceelasticsearch/Controller/ProductController.java
@@ -13,25 +13,24 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    // Get all products
+
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
-    // Get products by name
+
     @GetMapping("/search")
     public List<Product> getProductsByName(@RequestParam String name) {
         return productService.getProductsByName(name);
     }
 
-    // Get products filtered by price
+  
     @GetMapping("/filterByPrice")
     public List<Product> getProductsByPrice(@RequestParam double price) {
         return productService.getProductsByPrice(price);
     }
 
-    // Get products sorted by price
     @GetMapping("/sorted")
     public List<Product> getSortedProducts(@RequestParam String keyword) {
         return productService.getSortedProducts(keyword);
